aba5f533f2f5564bbe53589665d6fc19a7c9c713
Justin Barnyak <justin@barnyak.com>
2023-02-18 21:58:37 -0500

    master: adding a server side sync listener

--- bin/server.js
+++ bin/server.js
@@ -1,15 +1,22 @@
-#!/usr/bin/env node
-
-/**
- * @type {any}
- */
 const WebSocket = require('ws')
 const http = require('http')
 const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('./utils.js').setupWSConnection
-
+const Y = require('yjs');
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 1234
+
+const messageListener = (message) => {
+  const decoder = new TextDecoder();
+
+  // Check if the data is an array buffer
+  const data = message.data instanceof ArrayBuffer ? message.data : message.data.buffer;
+  const decodedMessage = decoder.decode(data);
+
+  let memoryDoc = Y.createDoc().extend('{}', decoedMessage);
+
+  console.log(memoryDoc.toJSON());
+};
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' })
@@ -26,6 +33,7 @@ server.on('upgrade', (request, socket, head) => {
    */
   const handleAuth = ws => {
     wss.emit('connection', ws, request)
+    wss.on('message', messageListener)
   }
   wss.handleUpgrade(request, socket, head, handleAuth)
 })
